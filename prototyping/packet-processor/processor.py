b5814f46ab0d710a5e90d8e714503bb196104632
Justin Smallwood <justin.d.smallwood@gmail.com>
2025-08-13 09:47:45 +1000

    20250813

--- prototyping/packet-processor/processor.py
+++ prototyping/packet-processor/processor.py
@@ -21,6 +21,7 @@ Main Questions:
 """
 
 import asyncio
+import enum
 import random
 from typing import Union
 
@@ -29,6 +30,14 @@ NUM_BLOCKS_IN_BUFFER = 5
 BUFFER_SIZE = NUM_PACKETS_FOR_PROCESSING * NUM_BLOCKS_IN_BUFFER
 BLOCK_SIZE = NUM_PACKETS_FOR_PROCESSING
 DROP_PROB = 0.001
+
+NUM_DROPPED_PACKETS = 0
+
+
+class BlockReadyReason(enum.Enum):
+    LAST_PACKET_ARRIVED = "Last packet arrived"
+    DROPPED_LAST_PACKET = "Dropped last packet"
+    NOT_READY = "Not ready"
 
 
 class Packet:
@@ -68,20 +77,31 @@ class RingBuffer:
         self.packet_arrived[next_idx] = 1
         self.last_arrived_seq = pkt.seq
 
-    def block_ready(self) -> bool:
+    def block_ready(self) -> tuple[bool, str]:
         # If we assume no out-of-order blocks, then arrival of later block means
         # that no more are coming for this block
-        return self.last_arrived_seq >= self.current_seq + self.block_size
+        if self.last_arrived_seq == self.current_seq + self.block_size:
+            return (True, BlockReadyReason.LAST_PACKET_ARRIVED)
+        elif self.last_arrived_seq >= self.current_seq + self.block_size:
+            return (True, BlockReadyReason.DROPPED_LAST_PACKET)
+        return (False, BlockReadyReason.NOT_READY)
 
-    def get_block(self) -> Union[None, list[Packet]]:
-        if self.block_ready():
+    def get_block(
+        self,
+    ) -> tuple[Union[None, tuple[list[Packet]]], Union[None, list[bool]], str]:
+        block_ready, reason = self.block_ready()
+
+        if block_ready:
             block = []
+            packet_arrived = []
             for _ in range(self.block_size):
                 if self.packet_arrived[self.tail]:
                     block.append(self.data[self.tail])
+                    packet_arrived.append(True)
                 else:
                     # in practice this would be a memset to zero for this memory
                     block.append(None)
+                    packet_arrived.append(False)
                 # if we set all packet_arrived to zero then we will
                 # know if new data has been written in this section.
                 # This is the only time we know for sure that there is
@@ -90,14 +110,15 @@ class RingBuffer:
                 self.tail = (self.tail + 1) % self.size
             self.current_seq += self.block_size
             self.head = (self.head + self.block_size) % self.size
-            return block
+            return block, packet_arrived, reason
         else:
-            return None
+            return None, None, ""
 
 
 async def producer(buffers: list[RingBuffer], channels: int) -> None:
     seqs = [0 for _ in range(channels)]
     first_packet = True
+    global NUM_DROPPED_PACKETS
     while True:
         c = random.randint(0, channels - 1)
         pkt = Packet(c, seqs[c])
@@ -118,6 +139,7 @@ async def producer(buffers: list[RingBuffer], channels: int) -> None:
                     buffers[c].current_seq = pkt.seq
         else:
             print(f"Dropping packet {pkt.seq} for channel {c}.")
+            NUM_DROPPED_PACKETS += 1
 
         await asyncio.sleep(0.01)
 
@@ -125,10 +147,13 @@ async def producer(buffers: list[RingBuffer], channels: int) -> None:
 async def consumer(buffers: list[list[RingBuffer]], channels: int) -> None:
     while True:
         for c in range(channels):
-            block = buffers[c].get_block()
+            block, packet_arrived, reason = buffers[c].get_block()
             if block:
                 # Send off to correlation / beamforming kernels
-                print(f"Sending block for channel {c}: {block}")
+                # packet_arrived will also get sent off.
+                print(
+                    f"Sending block for channel {c}: {block}. Packets: {packet_arrived}"
+                )
         await asyncio.sleep(0.05)
 
 
@@ -145,4 +170,9 @@ async def main(
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except:
+        print("Exiting...")
+        print(f"Number of dropped packets: {NUM_DROPPED_PACKETS}")
+        raise
