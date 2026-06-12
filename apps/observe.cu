6197bf6bae33c6bddaf51fa7be0654f03f029eb7
Justin Smallwood <justin.d.smallwood@gmail.com>
2026-06-12 12:21:15 +1000

    20260612

--- apps/observe.cu
+++ apps/observe.cu
@@ -121,8 +121,9 @@ int main(int argc, char *argv[]) {
       make_default_filename("eigendata", args.min_freq_channel,
                             num_lambda_channels, args.fpga_id_vec);
   HighFive::File eigen_file(eigen_filename, HighFive::File::Truncate);
-  auto eigen_writer = std::make_unique<HDF5EigenWriter<
-      Config::EigenvalueOutputType, Config::EigenvectorOutputType>>(eigen_file);
+  auto eigen_writer = nullptr; // std::make_unique<HDF5EigenWriter<
+  //      Config::EigenvalueOutputType,
+  //      Config::EigenvectorOutputType>>(eigen_file);
 
   auto fft_writer = std::make_unique<RedisBeamFFTWriter<Config::FFTOutputType>>(
       num_lambda_channels, nr_lambda_beams, nr_lambda_polarizations);
